f1987de3e092417fb2f6ff4da85599260f173621
ryoherisson <42690204+ryoherisson@users.noreply.github.com>
2022-01-06 10:46:52 +0900

    my first commit

---
+++ src/test/java/money/Money.java
@@ -0,0 +1,46 @@
+package money;
+
+class Money implements Expression {
+    protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    };
+
+    Expression plus(Money addend) {
+        return new Sum(this, addend);
+    }
+
+    public Money reduce(String to) {
+        int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        return new Money(amount / rate, to);
+    }
+
+    String currency() {
+        return currency;
+    };
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && currency().equals(money.currency());
+    }
+
+    public String toString() {
+        return amount + " " + currency;
+    }
+
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+}
